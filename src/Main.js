a0a5a4e4d824db757c13c69c9621ad8852e05f57
plaurion1989 <plaurion@protonmail.com>
2021-06-27 01:53:47 -0700

    added form

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import {CardColumns} from 'react-bootstrap';
+import { CardColumns, Form , Card} from 'react-bootstrap';
 
 
 
@@ -9,30 +9,58 @@ class Main extends React.Component {
     super(props)
     this.state = {
       data: [],
-      showModal: false
+      showModal: false,
+      searchHorn: 0
     }
     this.componentDidMount = () => {
       this.setState({
         data: this.props.importData
       })
     }
- 
+  }
+  filterData = () => {
+    const filterBeasts = (beast) => {
+      if (beast.horns === this.state.searchHorn) {
+        return true;
+      }
+      return false;
+    }
+
+    if (this.state.searchHorn) {
+      this.setState({ data: this.state.data.filter(filterBeasts) });
+    } else {
+      this.setState({ data: this.state.data });
+    }
+  }
+
+  handleHorns = (e) => {
+    let searchValue = parseInt(e.target.value);
+    this.setState({ searchHorn: searchValue }, this.filterData);
   }
   render() {
     return (
-      <> 
-      
-      <CardColumns>
-      {this.state.data.map(beast => {
-        return <HornedBeast
-          src={beast.image_url}
-          title={beast.title}
-          alt={beast.keyword}
-          description={beast.description}
-          showModal={this.state.showModal}
-          />
-      })}
-      </CardColumns>
+      <>
+        <Card style={{width:500}}>
+        <Form>
+          <Form.Label>Search By Horns</Form.Label>
+          <Form.Control onChange={this.handleHorns}as="select">
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+          </Form.Control>
+        </Form>
+        </Card>
+        <CardColumns>
+          {this.state.data.map(beast => {
+            return <HornedBeast
+              src={beast.image_url}
+              title={beast.title}
+              alt={beast.keyword}
+              description={beast.description}
+              showModal={this.state.showModal}
+            />
+          })}
+        </CardColumns>
       </>
     )
   }
